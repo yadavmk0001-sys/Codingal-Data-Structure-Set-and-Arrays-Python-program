4927c1476416617f06ae2ff193fdae1278c07331
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-06-16 21:44:22 +0530

    Add files via upload

---
+++ Prog2.py
@@ -0,0 +1,16 @@
+# Creating two sets
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+# Union(|)
+print("Union : ",set1 | set2)
+
+# Intersection (&)
+print("Intersection : ",set1 & set2)
+
+# Difference (-)
+print("Set 1 - Set2 : ",set1 - set2)
+print("Set2 - Set1 : ",set2 - set1)
+
+# Symmetric Difference (^)
+print("Symmetric difference : ",set1 ^ set2)

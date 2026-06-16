35718dcbdf2db28315570f3cab1477ca88b68e18
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-06-16 22:07:42 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,21 @@
+# Set1
+set1 = {'blue', 'green'}
+print("Set1 : ",set1)
+
+#Set2
+set2 = {'blue', 'yellow'}
+print("Set2 : ",set2)
+
+# Symmetric difference (^)
+print("Symmetric difference : ",set1 ^ set2)
+
+# Set1
+set1 = {1, 2, 3, 4, 5}
+print("Set1 : ",set1)
+
+# Set2
+set2 = {1, 5, 6, 7, 8, 9}
+print("Set2 : ",set2)
+
+# Symmetric difference (^)
+print("Symmetric Difference : ",set2 ^ set1)
